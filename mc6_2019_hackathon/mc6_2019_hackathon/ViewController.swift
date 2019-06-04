7cd26bb288da708ee89291c4736332a7fb1f3acc
Florian Kriegel <florian.kriegel@icloud.com>
2019-06-04 17:23:29 +0200

    init

---
+++ mc6_2019_hackathon/mc6_2019_hackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mc6_2019_hackathon
+//
+//  Created by Florian Kriegel on 04.06.19.
+//  Copyright © 2019 Florian Kriegel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
