118b6ef143e519213b4704963726accd5970d177
Florian Kriegel <florian.kriegel@icloud.com>
2019-06-07 14:08:48 +0200

    Add SpriteKit and two Joysticks

--- mc6_2019_hackathon/mc6_2019_hackathon/ViewController.swift
+++ mc6_2019_hackathon/mc6_2019_hackathon/ViewController.swift
@@ -7,16 +7,17 @@
 //
 
 import UIKit
-import AVKit
-import AVFoundation
+import SpriteKit
 
 class ViewController: UIViewController {
     var videoOn = false
     
     @IBOutlet weak var preView: UIImageView!
+    @IBOutlet weak var skView: SKView!
     @IBOutlet weak var video_btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.initJoystick()
         self.initSockets()
     }
     
@@ -33,6 +34,31 @@ class ViewController: UIViewController {
             }
         }
         self.videoOn = !self.videoOn
+    }
+    
+    func initJoystick() {
+        let transparent = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        skView.backgroundColor = transparent
+        
+        let scene = SKScene(size: skView.bounds.size)
+        scene.backgroundColor = transparent
+        scene.anchorPoint = CGPoint(x: scene.size.width / 2, y: scene.size.height / 2)
+        scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        let backgroundNode: SKSpriteNode = SKSpriteNode(color: transparent, size: skView.bounds.size)
+        
+        let leftStick = TLAnalogJoystick(withDiameter: 100)
+        leftStick.position = CGPoint(x: (backgroundNode.size.width / -2) + 150, y: (backgroundNode.size.height / -2) + 100)
+        
+        let rightStick = TLAnalogJoystick(withDiameter: 100)
+        rightStick.position = CGPoint(x: (backgroundNode.size.width / 2) - 150, y: (backgroundNode.size.height / -2) + 100)
+
+        backgroundNode.addChild(leftStick)
+        backgroundNode.addChild(rightStick)
+        
+        scene.addChild(backgroundNode)
+        
+        skView.presentScene(scene)
     }
     
     func initSockets() {
