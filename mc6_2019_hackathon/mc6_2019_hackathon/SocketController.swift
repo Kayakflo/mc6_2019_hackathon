bf513c951558b552b1d7d0284bbbcdffdadc957c
Florian Kriegel <florian.kriegel@icloud.com>
2019-06-07 10:58:41 +0200

    Redesign Main View and add function to enable or disable video manually

--- mc6_2019_hackathon/mc6_2019_hackathon/SocketController.swift
+++ mc6_2019_hackathon/mc6_2019_hackathon/SocketController.swift
@@ -21,21 +21,26 @@ class SocketController: VideoFrameDecoderDelegate {
         if self.commandClient == nil {
             commandClient = UDPClient(address: "192.168.10.1", port: 8889)
             commandClient.send(string: "command")
-            commandClient.send(string: "streamon")
-            //commandClient.send(string: "takeoff")
-            //commandClient.send(string: "land")
-            commandClient.close()
         }
+    }
+    
+    func deinitCommandClient() {
+        self.commandClient.close()
+        self.commandClient = nil
     }
     
     func initStatusServer() {
         statusServer = UDPServer(address: "0.0.0.0", port: 8890)
-        while true {
-            let msg = statusServer.recv(1024)
+        DispatchQueue.global(qos: .userInteractive).async {
+            while true {
+                let msg = self.statusServer.recv(1024)
+            }
         }
     }
     
     func initStreamServer(onImage: @escaping (_ image: CGImage) -> ()) {
+        if streamServer != nil { return }
+        self.commandClient.send(string: "streamon")
         self.onImage = onImage
         streamServer = UDPServer(address: "0.0.0.0", port: 11111)
         let dec: VideoFrameDecoder = VideoFrameDecoder()
@@ -56,7 +61,12 @@ class SocketController: VideoFrameDecoderDelegate {
         }
     }
     
-    func receivedDisplayableFrame(_ frame: CVPixelBuffer) {
+    func deinitStreamServer() {
+        self.commandClient.send(string: "streamoff")
+        self.streamServer = nil
+    }
+    
+    internal func receivedDisplayableFrame(_ frame: CVPixelBuffer) {
         var cgImage: CGImage?
         VTCreateCGImageFromCVPixelBuffer(frame, options: nil, imageOut: &cgImage)
 
--- mc6_2019_hackathon/mc6_2019_hackathon/ViewController.swift
+++ mc6_2019_hackathon/mc6_2019_hackathon/ViewController.swift
@@ -11,15 +11,36 @@ import AVKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    var videoOn = false
+    
     @IBOutlet weak var preView: UIImageView!
+    @IBOutlet weak var video_btn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.initSockets()
+    }
+    
+    @IBAction func video_btn(_ sender: Any) {
+        if videoOn {
+            SocketController.singleton.deinitStreamServer()
+            self.video_btn.setBackgroundImage(UIImage(named: "video_off"), for: .normal)
+        } else {
+            self.video_btn.setBackgroundImage(UIImage(named: "video_on"), for: .normal)
+            SocketController.singleton.initStreamServer { (cgImage) in
+                DispatchQueue.main.async {
+                    self.preView.image = UIImage(cgImage: cgImage)
+                }
+            }
+        }
+        self.videoOn = !self.videoOn
+    }
+    
+    func initSockets() {
+        SocketController.singleton.initCommandClient()
+        SocketController.singleton.initStatusServer()
     }
 
     @IBAction func liftoff(_ sender: Any) {
-        SocketController.singleton.initCommandClient()
-        sleep(5)
         SocketController.singleton.initStreamServer { (cgImage) in
             DispatchQueue.main.async {
                 self.preView.image = UIImage(cgImage: cgImage)
